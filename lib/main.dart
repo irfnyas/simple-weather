0fd20681ec99a6a01857e2adee204babc014be93
Irfan Yas <irfanyasiras@gmail.com>
2022-07-13 00:45:05 +0700

    feat(env): add .env for secret key

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:simple_weather/data/cache.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:simple_weather/domain/util/constant.dart';
 import 'package:simple_weather/domain/util/router.dart';
 import 'package:simple_weather/domain/util/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  initServiceLocator();
 
-  final isLoggedIn = await Cache.isLoggedIn();
-  runApp(MyApp(isLoggedIn: isLoggedIn));
+  initServiceLocator();
+  await dotenv.load(fileName: ".env");
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key, required this.isLoggedIn}) : super(key: key);
-  final bool isLoggedIn;
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Colors.orange.shade700;
     return MaterialApp.router(
       title: appName,
       theme: ThemeData(
