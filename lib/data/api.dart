6f9d5fb8fa32a5c3ff027e7d02d2a125782b8bfd
Irfan Yas <irfanyasiras@gmail.com>
2022-07-22 11:55:37 +0700

    reformat: move getr fun to middleware
    api.dart should consist request methods only

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -3,25 +3,13 @@ import 'package:http/http.dart';
 import 'package:simple_weather/domain/util/api_middleware.dart';
 import 'package:simple_weather/domain/util/constant.dart';
 
-Future<Response?> getr(String url, Map<String, String>? queries) async {
-  Response? res;
-  try {
-    final uri = Uri.parse(url).replace(queryParameters: queries);
-    final req = await get(uri);
-    res = await midCheck(req);
-  } catch (e) {
-    await midError(e);
-  }
-  return res;
-}
-
-Future<Response?> apiGetProvinces() => getr('$urlWilayah/provinces.json', null);
+Future<Response?> apiGetProvinces() => getr('$urlWilayah/provinces.json');
 
 Future<Response?> apiGetCities(String provinceId) =>
-    getr('$urlWilayah/regencies/$provinceId.json', null);
+    getr('$urlWilayah/regencies/$provinceId.json');
 
 Future<Response?> apiGetForecast(String lat, String lng) =>
-    getr(urlOpenWeather, {
+    getr(urlOpenWeather, queries: {
       'appid': dotenv.get('OPEN_WEATHER_KEY', fallback: ''),
       'lat': lat,
       'lon': lng,
--- lib/domain/util/api_middleware.dart
+++ lib/domain/util/api_middleware.dart
@@ -2,6 +2,18 @@ import 'package:http/http.dart';
 import 'package:simple_weather/domain/util/constant.dart';
 import 'package:simple_weather/domain/util/dialog_manager.dart';
 
+Future<Response?> getr(String url, {Map<String, String>? queries}) async {
+  Response? res;
+  try {
+    final uri = Uri.parse(url).replace(queryParameters: queries);
+    final req = await get(uri);
+    res = await midCheck(req);
+  } catch (e) {
+    await midError(e);
+  }
+  return res;
+}
+
 Future<Response?> midCheck(Response? res) async {
   switch (res?.statusCode ?? -1) {
     case 200:
