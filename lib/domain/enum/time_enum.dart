d1271c380bd47e6b44ea8f396c016aa3780970c8
Irfan Yas <irfanyasiras@gmail.com>
2022-07-22 08:46:40 +0700

    reformat: enhanced time enum

--- lib/domain/enum/time_enum.dart
+++ lib/domain/enum/time_enum.dart
@@ -1,18 +1,11 @@
 import 'package:simple_weather/domain/util/constant.dart';
 
-enum Time { morning, afternoon, evening, night }
+enum Time {
+  morning(textTimeMorning),
+  afternoon(textTimeAfternoon),
+  evening(textTimeEvening),
+  night(textTimeNight);
 
-extension TimeExt on Time {
-  String get greet {
-    switch (this) {
-      case Time.morning:
-        return textTimeMorning;
-      case Time.afternoon:
-        return textTimeAfternoon;
-      case Time.evening:
-        return textTimeEvening;
-      case Time.night:
-        return textTimeNight;
-    }
-  }
+  final String greet;
+  const Time(this.greet);
 }
